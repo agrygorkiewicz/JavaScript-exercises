e637bd5e49f41b3b2e4e7ba95e3e836d889dcf17
agrygorkiewicz <aneta.grygorkiewicz@gmail.com>
2021-01-20 22:32:51 +0100

    exercise 1 and 2

---
+++ Add-Delete Product/js/app.js
@@ -0,0 +1,53 @@
+// Na stronie znajduje się lista zakupów. Popatrz na HTML i zobacz jak lista jest zbudowana.
+// Dopisz odpowiednią obsługę eventów tak, aby:
+//
+// Po kliknięciu przycisku pierwszego do listy został dopisany nowy produkt - Chleb.
+//
+// Po kliknięciu przycisku drugiego z listy był usuwany ostatni element.
+// Jeżeli nie ma czego usunąć (lista jest pusta) powinna pojawić się taka informacja w konsoli: "Lista jest pusta".
+//
+// Po kliknięciu przycisku trzeciego na końcu listy był dodawany nowy produkt, który jest klonem drugiego produktu.
+// Jeżeli na liście jest tylko jeden lub mniej produktów,
+// w konsoli powinna pojawić się informacja: "Lista zawiera jeden lub mniej produktów".
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button1 = document.querySelector("#button-1");
+    const button2 = document.querySelector("#button-2");
+    const button3 = document.querySelector("#button-3");
+    const list = document.querySelector(".list-group-item");
+    const li = document.querySelectorAll("li");
+    const container = document.querySelector(".container");
+    const ul = document.querySelector("ul");
+    const shopping = document.querySelector("#shopping-list");
+
+    button1.addEventListener("click", e => {
+
+        const li = document.createElement("li");
+        li.classList.add("list-group-item");
+        li.innerText = "Herbata";
+        shopping.append(li);
+
+    })
+
+    button2.addEventListener("click", e => {
+        if (shopping.children.length > 0) {
+            const last = shopping.lastChild;
+            last.remove();
+        } else {
+            console.log("Lista jest pusta");
+        }
+
+
+    })
+    button3.addEventListener("click", e => {
+        if (shopping.children.length >= 2) {
+            const clone = shopping.children[1].cloneNode(true); // true - klonuje z zawartością false - klonuje samą komórkę
+            shopping.append(clone);
+        } else {
+            console.log("Lista zawiera jeden lub mniej produktów");
+
+        }
+
+    })
+
+})
---
+++ Data-text Button/js/app.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelectorAll(".btn");
+    const alert = document.querySelector(".alert");
+
+    for (let el of btn) {
+    el.addEventListener("click", e => {
+            alert.innerHTML = el.dataset.text;
+        })
+    }
+
+});
